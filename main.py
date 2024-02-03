93394ebdaa25fe1e4363add848733d8130811380
schneider <Yobobis@gmail.com>
2024-02-03 23:49:38 +0100

    early bar & phrase generator

--- main.py
+++ main.py
@@ -17,8 +17,27 @@ MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
                       # automatically)
 MyMIDI.addTempo(track, time, tempo)
 
-for i, pitch in enumerate(degrees2):
-    MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
+bar = []
+i = 0
+phrase = mg_lib.generate_phrase(60)
+
+# NoteAdder 1 (best result IMO)
+for j in range(20):
+    if j % 3 != 0:
+        bar = mg_lib.generate_bar(mg_lib.major_scale(60 + ((j % 2) * 12)))
+    for note in bar:
+        i += note.duration
+        MyMIDI.addNote(track, channel, note.pitch, i, note.duration, volume)
+
+
+# NoteAdder 2
+#for note in phrase:
+#    i += note.duration
+#    MyMIDI.addNote(track, channel, note.pitch, i, note.duration, volume)
+
+# NoteAdder 3
+#for i, pitch in enumerate(degrees2):
+#    MyMIDI.addNote(track, channel, pitch, time + i, duration, volume)
 
 with open("output.mid", "wb") as output_file:
     MyMIDI.writeFile(output_file)
--- mg_lib.py
+++ mg_lib.py
@@ -1,5 +1,9 @@
 # Music Generator Library
 
+from midiutil import MIDIFile
+from note import Note
+import random
+
 def major_chord(root):
     return [root, root + 4, root + 7]
 
@@ -14,3 +18,44 @@ def major_scale(root):
         else:
             res.append(res[len(res) - 1] + 1)
     return res
+
+def minor_scale(root):
+    res = [root]
+    for i in range(6):
+        if (i != 1):
+            res.append(res[len(res) - 1] + 2)
+        else:
+            res.append(res[len(res) - 1] + 1)
+    return res
+
+# Generate a 4 beats bar
+# scale is a list of allowed pitches
+def generate_bar(scale):
+    res = []
+
+    idx = random.randrange(0, len(scale))
+    current = scale[idx]
+
+    for i in range(4):
+
+        current = random.choice(scale)
+
+        r = random.randint(0, 3)
+        if r == 0:
+            res.append(Note(random.choice(scale), 1, 100))
+        elif r == 1:
+            for i in range(2):
+                res.append(Note(random.choice(scale), 0.5, 100))
+        elif r == 2:
+            for i in range(4):
+                res.append(Note(random.choice(scale), 0.25, 100))
+        elif r == 3:
+            res.append(Note(random.choice(scale), 1, 0))
+    return res
+
+def generate_phrase(root):
+    bar1 = generate_bar(major_scale(root))
+    bar2 = generate_bar(minor_scale(root))
+    bar3 = bar2
+    bar4 = generate_bar(major_scale(root))
+    return bar1 + bar2 + bar3 + bar4

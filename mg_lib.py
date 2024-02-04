0023044b2656cd23785cfa8c61f9542789b39137
schneider <Yobobis@gmail.com>
2024-02-04 18:30:53 +0100

    added scale class + added rythm, chord and melody adders

--- mg_lib.py
+++ mg_lib.py
@@ -2,6 +2,7 @@
 
 from midiutil import MIDIFile
 from note import Note
+from scale import Scale
 import random
 
 def major_triad(root):
@@ -59,3 +60,65 @@ def generate_phrase(root):
     bar3 = generate_bar(major_scale(root))
     bar4 = generate_bar(major_scale(root))
     return bar1 + bar2 + bar3 + bar4
+
+# 1 5 6 4
+# 6 4 1 5
+# 1 6 4 5
+# 4 5 3 6
+# 1 5 4 5
+def generate_chord_progression(root):
+    scale = Scale(root, False)
+    res = [
+        scale.getTriad(random.choice([1, 6])),
+        scale.getTriad(random.randint(4, 6)),
+        scale.getTriad(random.randint(1, 6)),
+        scale.getTriad(random.randint(4,5))]
+    return res
+
+# 36 = kick
+# 38 = snare
+# 42 = hihat (closed)
+# -1 = silence
+# every 1/4 must be filled
+def generate_drum_bar():
+    res = []
+    for i in range(4):
+        for j in range(4):
+            if i == 0 and j == 0:
+                res.append(36)
+            elif j == 0:
+                if random.randint(0, 1) == 1:
+                    res.append(38)
+                else:
+                    res.append(42)
+            else:
+                if random.randint(0, 1) == 1:
+                    res.append(42)
+                else:
+                    res.append(-1)
+    return res
+
+def generate_crazy_drum_bar():
+    res = []
+    for i in range(16):
+        r = random.randint(0, 4)
+        if r == 1:
+            res.append(36)
+        elif r == 2:
+            res.append(38)
+        elif r == 3:
+            res.append(42)
+        elif r == 4:
+            res.append(-1)
+    return res
+
+# chord = the current chord played along the bar
+def generate_melody(chord):
+    res = []
+
+    for i in range(8):
+        if i == 0:
+            res.append(chord[0] + 12)
+            continue
+        res.append(random.choice(chord) + 12)
+    return res
---
+++ scale.py
@@ -0,0 +1,41 @@
+
+class Scale():
+    def __init__(self, root, isMinor = False) -> None:
+        self.root = root
+        self.isMinor = isMinor
+        self.getScale()
+
+    def getScale(self):
+        self.scale = [self.root]
+        nextRoot = self.root + 12
+        if self.isMinor:
+            self.scale.append(self.root + 2)
+            self.scale.append(self.root + 3)
+            self.scale.append(self.root + 5)
+            self.scale.append(self.root + 7)
+            self.scale.append(self.root + 8)
+            self.scale.append(self.root + 10)
+            self.scale.append(nextRoot + 2)
+            self.scale.append(nextRoot + 3)
+            self.scale.append(nextRoot + 5)
+            self.scale.append(nextRoot + 7)
+            self.scale.append(nextRoot + 8)
+            self.scale.append(nextRoot + 10)
+            self.scale.append(nextRoot)
+        else:
+            self.scale.append(self.root + 2)
+            self.scale.append(self.root + 4)
+            self.scale.append(self.root + 5)
+            self.scale.append(self.root + 7)
+            self.scale.append(self.root + 9)
+            self.scale.append(self.root + 11)
+            self.scale.append(nextRoot)
+            self.scale.append(nextRoot + 2)
+            self.scale.append(nextRoot + 4)
+            self.scale.append(nextRoot + 5)
+            self.scale.append(nextRoot + 7)
+            self.scale.append(nextRoot + 9)
+            self.scale.append(nextRoot + 11)
+
+    def getTriad(self, triad):
+        return [self.scale[triad], self.scale[triad + 3], self.scale[triad + 5]]
